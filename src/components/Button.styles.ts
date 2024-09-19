930da60b974c4cfcbe234136bbc4a7f7e254b680
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-19 19:14:09 +0100

    Refactor global styles and add font imports

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -10,5 +10,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${(props) => props.theme.primary};
+  border-radius: 4px;
+  border: none;
+  margin: 8px;
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["white"]};
 `;
