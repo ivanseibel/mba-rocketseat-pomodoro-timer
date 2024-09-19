b8f01eaac8e412cd1e55d30d9bdb54456baa0fa0
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-19 18:00:09 +0100

    Refactor ButtonContainer styles to use theme primary color

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,31 +6,9 @@ type ButtonContainerProps = {
   variant: ButtonVariant;
 };
 
-const buttonVariants = {
-  primary: {
-    color: "white",
-    backgroundColor: "blue",
-  },
-  secondary: {
-    color: "white",
-    backgroundColor: "gray",
-  },
-  success: {
-    color: "white",
-    backgroundColor: "green",
-  },
-  danger: {
-    color: "white",
-    backgroundColor: "red",
-  },
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${({ variant }) => css`
-    color: ${buttonVariants[variant].color};
-    background-color: ${buttonVariants[variant].backgroundColor};
-  `}
+  background-color: ${(props) => props.theme.primary};
 `;
