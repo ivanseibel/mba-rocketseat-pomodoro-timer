98f4b47c2c6d7a557cc24b16f08248362d46137f
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-10-11 20:49:32 +0100

    Refactor package.json and pnpm-lock.yaml to add immer as a dependency

--- src/reducers/tasks/reducers.ts
+++ src/reducers/tasks/reducers.ts
@@ -1,3 +1,5 @@
+import { produce } from "immer";
+
 import { Task } from "../../contexts/TasksContext";
 import { ActionTypes } from "./actions";
 
@@ -10,44 +12,42 @@ type TasksState = {
 export function tasksReducer(state: TasksState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_TASK:
-      return {
-        ...state,
-        tasks: [...state.tasks, action.payload.task],
-        activeTaskId: action.payload.task.id,
-        amountSecondsPassed: 0,
-      };
-    case ActionTypes.STOP_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          if (task.id === state.activeTaskId) {
-            return {
-              ...task,
-              stoppedAt: new Date(),
-            };
-          }
+      return produce(state, (draft) => {
+        draft.tasks.push(action.payload.task);
+        draft.activeTaskId = action.payload.task.id;
+        draft.amountSecondsPassed = 0;
+      });
+    case ActionTypes.STOP_TASK: {
+      const currentTaskIndex = state.tasks.findIndex(
+        (task) => task.id === state.activeTaskId,
+      );
 
-          return task;
-        }),
-        activeTaskId: null,
-        amountSecondsPassed: 0,
-      };
-    case ActionTypes.FINISH_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          if (task.id === state.activeTaskId) {
-            return {
-              ...task,
-              finishedAt: new Date(),
-            };
-          }
+      if (currentTaskIndex === -1) {
+        return state;
+      }
 
-          return task;
-        }),
-        activeTaskId: null,
-        amountSecondsPassed: 0,
-      };
+      return produce(state, (draft) => {
+        draft.tasks[currentTaskIndex].stoppedAt = new Date();
+        draft.activeTaskId = null;
+        draft.amountSecondsPassed = 0;
+      });
+    }
+
+    case ActionTypes.FINISH_TASK: {
+      const currentTaskIndex = state.tasks.findIndex(
+        (task) => task.id === state.activeTaskId,
+      );
+
+      if (currentTaskIndex === -1) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.tasks[currentTaskIndex].finishedAt = new Date();
+        draft.activeTaskId = null;
+        draft.amountSecondsPassed = 0;
+      });
+    }
     case ActionTypes.UPDATE_AMOUNT_SECONDS_PASSED:
       return {
         ...state,
